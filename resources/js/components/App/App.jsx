9e904267970ed6dc01bbcc8268b8052469c825b2
FredyHenao <jhongallego45y@hotmail.com>
2018-10-05 10:13:45 -0500

    Create component App

---
+++ resources/js/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from "../Header";
+import Main from '../Main';
+
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                <Header />
+                <Main />
+            </div>
+        );
+    }
+}
+
+if(document.getElementById('app')){
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
