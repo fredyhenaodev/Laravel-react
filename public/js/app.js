f50394c207c68263f1893add88302bf795220543
FredyHenao <jhongallego45y@hotmail.com>
2018-12-23 10:20:33 -0500

    Componentes Statefull y Stateless

--- public/js/app.js
+++ public/js/app.js
@@ -84309,47 +84309,23 @@ exports.unstable_unsubscribe = unstable_unsubscribe;
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__assets_jss_components_header__ = __webpack_require__(243);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_jss__ = __webpack_require__(9);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_jss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_jss__);
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
 
 
 // Import React-JSS
 
 
-var Header = function (_Component) {
-    _inherits(Header, _Component);
-
-    function Header() {
-        _classCallCheck(this, Header);
-
-        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
-    }
-
-    _createClass(Header, [{
-        key: 'render',
-        value: function render() {
-            var classes = this.props.classes;
-
-            return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-                'header',
-                { className: classes.root },
-                __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
-                    'h1',
-                    { className: classes.logo },
-                    'React'
-                )
-            );
-        }
-    }]);
-
-    return Header;
-}(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
+var Header = function Header(_ref) {
+    var classes = _ref.classes;
+    return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+        'header',
+        { className: classes.root },
+        __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+            'h1',
+            { className: classes.logo },
+            'React'
+        )
+    );
+};
 
 /* harmony default export */ __webpack_exports__["a"] = (__WEBPACK_IMPORTED_MODULE_2_react_jss___default()(__WEBPACK_IMPORTED_MODULE_1__assets_jss_components_header__["a" /* default */])(Header));
 
--- resources/js/components/Header/index.jsx
+++ resources/js/components/Header/index.jsx
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Styles from '../../assets/jss/components/header';
 // Import React-JSS
 import injectSheet from 'react-jss'
 
 
-class Header extends Component{
-    render(){
-        const {classes} = this.props;
-        return(
-            <header className={classes.root}>
-                <h1 className={classes.logo}>React</h1>
-            </header>
-        );
-    }
-}
+const Header = ({classes}) => (
+    <header className={classes.root}>
+        <h1 className={classes.logo}>React</h1>
+    </header>
+)
 
 export default injectSheet(Styles)(Header);
